4efac5daf7e6ebf7ed9dde6bc657062533d2653d
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-05-15 17:11:03 +0800

    update comment in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+//database connection details from environmental variable
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_DATABASENAME, process.env.DB_USERNAME, process.env.DB_PW, {
